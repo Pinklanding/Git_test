eb7d44619405f895770354876684c108d43e5623
ChunGuan <chun_guan@163.com>
2019-12-28 11:55:52 +0800

    Initial Commit

---
+++ Git_test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Git_test
+//
+//  Created by 关春 on 2019/12/28.
+//  Copyright © 2019 关春. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
